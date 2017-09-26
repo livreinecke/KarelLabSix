00982756e2013a0e6999b98d97ed676b4a458af1
livreinecke <livreinecke@outlook.com>
2017-09-26 12:50:18 -0700

    lab 6

--- Problem1.java
+++ Problem1.java
@@ -11,9 +11,36 @@ public class Problem1 extends Robot
         super(st, av, dir, numBeepers);
     }
     
+    int[] columns = new int [7];
+    public int beepers;
+    
     public void sortBeepers() {
+        move();
+        turnLeft();
+        checkBeepersInRow();
+        recordRowNmbers();
+        collectBeepers();
+        layDownBeepersOrder();
+    }
+    
+    public void checkBeepersInRow(){
         
     }
    
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    
+    public void localVariable() {
+        int beepers = 4;
+        
+        while (beepers > 0) {
+             move();
+             beepers--;
+        }
+
+    }
 }
 
